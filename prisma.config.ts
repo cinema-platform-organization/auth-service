984e19ccf49f582e4134395c254ec2598a52b2bf
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2026-08-22 20:55:26 +0300

    Add env validation

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,4 +1,3 @@
-import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
@@ -7,6 +6,6 @@ export default defineConfig({
 		path: "prisma/migrations",
 	},
 	datasource: {
-		url: env("POSTGRES_URI"),
+		url: env("DATABASE_URI"),
 	},
 });
